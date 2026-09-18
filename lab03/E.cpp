fa7c3fb52e0461895b9a74447849207a164e6b27
Shyngys <seidualiev01@gmail.com>
2026-09-18 11:31:55 +0500

    5

---
+++ lab03/E.cpp
@@ -0,0 +1,55 @@
+/*
+Problem E: Patchwork Staccato II
+Number of elements between bounds queries
+*/
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int countRange(const vector<int>& a, int l, int r) {
+    if (l > r) {
+        return 0;
+    }
+
+    auto left = lower_bound(a.begin(), a.end(), l);
+    auto right = upper_bound(a.begin(), a.end(), r);
+
+    return right - left;
+}
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    // Sort the array for binary search
+    sort(a.begin(), a.end());
+
+    while (q--) {
+        int l1, r1, l2, r2;
+        cin >> l1 >> r1 >> l2 >> r2;
+
+        // Count elements in both segments
+        int answer = countRange(a, l1, r1)
+                   + countRange(a, l2, r2);
+
+        // Find intersection of the two segments
+        int overlapLeft = max(l1, l2);
+        int overlapRight = min(r1, r2);
+
+        // Remove elements counted twice
+        answer -= countRange(a, overlapLeft, overlapRight);
+
+        cout << answer << '\n';
+    }
+
+    return 0;
+}
